02057f4b3bb038727ef878794158a9e23270ca87
denortlui <denortlui@alum.us.es>
2023-05-10 19:43:27 +0200

    Test de unidad para la tarea 92, que comprueban el código de la tarea 91.

---
+++ src/test/java/us/muit/fs/a4i/test/persistence/FontTest.java
@@ -0,0 +1,80 @@
+package us.muit.fs.a4i.test.persistence;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.util.logging.Logger;
+
+
+/**
+ * Test de la clase Font
+ * 
+ * @author Denis Ortega Luis
+ * @see org.junit.jupiter.api.Tag
+ *
+ */
+
+class FontTest {
+	private static Logger log = Logger.getLogger(FontTest.class.getName());
+	
+	static Font underTest;
+	
+	/*Test que comprueba que existe un color por defecto y que su formato es válido*/
+	@Test 
+	void testColorPorDefecto(){
+		underTest = new Font();
+		String colorDevuelto;
+		
+		try {
+			//Compruebo que tiene un valor por defecto
+			assertNotNull(underTest.getColor(), "Debe tener un color por defecto");
+		
+			colorDevuelto = undetTest.getColor();
+			
+		}catch(Exception e) {
+			fail("El tipo de dato color no es de tipo String o el formato no es válido");
+			e.printStackTrace();
+		}
+		
+		//Compruebo el formato del color por defecto
+		try {
+			Color.decode(colorDevuelto);
+			
+		}catch(NumberFormatException e) {
+			fail("El formato del color no es un formato válido");
+			e.printStackTrace();
+		}
+	}
+	
+	
+	@Test
+	void testFuente() {
+		underTest = new Font();
+		
+		//Compruebo que se devuelve el mismo tipo
+		assertEquals(underTest.getFont(), underTest, "El objeto devuelto debe ser la misma fuente")
+	}
+	
+	@Test
+	void testSize() {
+		underTest = new Font();
+		
+		//Compruebo que se tenga un valor por defecto
+		assertNotNull(underTest.getSize(), "Debe tener un valor por defecto");
+		
+		//Compruebo que sea del tipo int
+		assertThat(underTest.getSize(), is(instanceOf(Integer.class))), "Deberia ser del tipo Integer.");
+	}
+	
+	@Test
+	void testNombre() {
+		underTest = new Font();
+		
+		//Compruebo que se tenga un valor por defecto
+		assertNotNull(underTest.getName(), "Debe tener un valor por defecto");
+		
+		//Compruebo que sea del tipo int
+		assertThat(underTest.getName(), is(instanceOf(String.class))), "Deberia ser del tipo Integer.");
+	}
+}
